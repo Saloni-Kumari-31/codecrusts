08d69399d9144f040d8c5ac1f9d5a791d4b8efe5
solujr <saloni9968066@gmail.com>
2025-03-12 15:11:17 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXsGf1js6GUEZBWI5_FCeZ4pIIqafrBJU",
+    authDomain: "fir-76325.firebaseapp.com",
+    projectId: "fir-76325",
+    storageBucket: "fir-76325.firebasestorage.app",
+    messagingSenderId: "1082729740137",
+    appId: "1:1082729740137:web:75bc7db7b50f4db1002ff8",
+    measurementId: "G-XE10HVPHQ9"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
